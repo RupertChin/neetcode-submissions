1c1970bf007752c8d35277e3e0faaad9042f5fdb
Robert Yin <64672923+RupertChin@users.noreply.github.com>
2026-05-25 15:14:43 -0400

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    def isSameTree(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        if not root1 and not root2:
+            return True
+        elif not root1 or not root2:
+            return False
+        
+        if root1.val != root2.val:
+            return False
+        
+        return self.isSameTree(root1.left, root2.left) and self.isSameTree(root1.right, root2.right)
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root: # subRoot is never None
+            return False
+
+        if self.isSameTree(root, subRoot):
+            return True
+        
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        
+        """
+        Another potential approach:
+        -  traversal to turn both trees into a string/array of vals
+            - can be any kind as long as it's same (e.g. preorder)
+            - ensure to include notation for null value
+        - compare root string to see if subroot string is a substr
+            - if yes then it's a subroot
+        """
+        
