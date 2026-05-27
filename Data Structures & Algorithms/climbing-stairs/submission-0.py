ea8771b3fca4d9f8db7b93557d2f4183c1d369fb
Robert Yin <64672923+RupertChin@users.noreply.github.com>
2026-05-26 22:30:17 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        """
+        Approach, backwards dp
+        """
+        if (n == 1):
+            return 1 # edge case
+
+        dp = [0] * n
+        dp[-1] = 1
+        dp[-2] = 2
+
+        for i in range(n-3, -1, -1):
+            dp[i] = dp[i+1] + dp[i+2]
+
+        return dp[0]
