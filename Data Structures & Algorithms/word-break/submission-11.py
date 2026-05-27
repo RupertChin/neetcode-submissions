750c26797645073a30a34911ef10645205b9b80f
Robert Yin <64672923+RupertChin@users.noreply.github.com>
2026-05-27 19:53:08 -0400

    Add: word-break - submission-11

---
+++ Data Structures & Algorithms/word-break/submission-11.py
@@ -0,0 +1,80 @@
+class TrieNode:
+    val: str
+    is_terminal: bool
+    children: dict
+
+    def __init__(self, v: str = ""):
+        self.val = v
+        self.is_terminal = False
+        self.children = {}
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        """
+        Approach:
+        - dp problem, multiple words can have same prefix
+            - either select one or other
+        - trie? more efficient lookup
+            - search per char, recursive call per terminal node
+            - top down, memoization
+
+        Approach 2:
+        - same as before but use hashset for lookup instead of trie
+        """
+
+        # build trie
+        trie = TrieNode()
+        for word in wordDict:
+            curr = trie
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode(c)
+                curr = curr.children[c]
+            curr.is_terminal = True
+        
+        mem = [None] * (len(s) + 1) # is memoization really required?
+        def _wordBreak(i: int) -> bool:
+            nonlocal mem
+
+            if i == len(s):
+                return True
+
+            # need memoization check? 
+            if mem[i] is not None:
+                return mem[i]
+            
+            cur_node = trie
+            while i < len(s) and s[i] in cur_node.children:
+                cur_node = cur_node.children[s[i]]
+                i += 1
+                if cur_node.is_terminal:
+                    if _wordBreak(i):
+                        mem[i] = True
+                        return True
+            
+            mem[i-1] = False
+            return False
+        
+        return _wordBreak(0)
+
+        # wordDict = set(wordDict)
+        # mem = [None] * (len(s) + 1)
+        
+        # def _wordBreak(i: int) -> bool:
+        #     nonlocal mem
+        #     if i >= len(s):
+        #         return True
+
+        #     if mem[i] is not None:
+        #         return mem[i]
+
+        #     for j in range(i+1, len(s)+1):
+        #         if s[i:j] in wordDict:
+        #             if _wordBreak(j):
+        #                 mem[j] = True
+        #                 return True
+            
+        #     mem[i] = False
+        #     return False
+        
+        # return _wordBreak(0)
